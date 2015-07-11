bc248d3a2144a3551cfa9fbb5feffc9ca885e73c
Clinton Andrews <candrews800@gmail.com>
2015-07-11 18:49:10 -0400

    setup default routing

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.get('/test', function(req, res) {
-   res.send('Test page');
-});
-
 var server = app.listen(3000, function () {
     console.log('Example app listening at http://zipnode.app:3000');
 
